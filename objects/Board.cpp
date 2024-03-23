47381d6001e17c7fc8262404cfcac270b2a8f440
Bobislav <bobo.svilenov@gmail.com>
2024-03-23 09:50:58 +0200

    Board function deffinitions

--- objects/Board.cpp
+++ objects/Board.cpp
@@ -1,3 +1,33 @@
 #include "Board.hpp"
 
+Board::Board() 
+{
+
+}
+
+void Board::move()
+{
+
+}
+
+void Board::subdivide()
+{
+
+}
+
+void Board::checkWin()
+{
+
+}
+
+void Board::win()
+{
+
+}
+
+void Board::drawBoard()
+{
+
+}
+
 
--- objects/Board.hpp
+++ objects/Board.hpp
@@ -3,6 +3,24 @@
 
 class Board
 {
+private:
+
+public:
+	int winner = 0;
+	int mvs = [[0, 0, 0],
+						 [0, 0, 0],
+						 [0, 0, 0]];
+	
+	bool subd = false;
+
+	Board();
+
+	void move();
+	void subdivide();
+	void checkWin();
+	void win();
+
+	void drawBoard();
 
 };
 
