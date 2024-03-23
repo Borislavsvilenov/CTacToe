8249b1c335216ebb3b47fc214996b12937842350
Bobislav <bobo.svilenov@gmail.com>
2024-03-23 15:44:31 +0200

    done with nullptrs for now

--- build/CMakeFiles/CTacToe.dir/objects/Board.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/CTacToe.dir/objects/Board.cpp.o: \
-  /Users/borislavsvilenov/Projects/CTacToe/objects/Board.cpp \
-  /Users/borislavsvilenov/Projects/CTacToe/objects/Board.hpp \
-  /opt/homebrew/Cellar/raylib/5.0/include/raylib.h \
-  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdarg.h
